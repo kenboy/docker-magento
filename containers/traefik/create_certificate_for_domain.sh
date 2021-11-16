3b76fbaedb65255d1b05449df59d9a2a098f3878
Evgenii Kalashnikov <e.kalashnikov@prola.ru>
2021-11-16 21:06:03 +0400

    Update create_certificate_for_domain.sh

--- containers/traefik/create_certificate_for_domain.sh
+++ containers/traefik/create_certificate_for_domain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkcert -cert-file certs/local.crt -key-file certs/local.key "magento.local"
+mkcert -cert-file certs/local.crt -key-file certs/local.key "magento.local" "traefik.local" "mailcatcher.local"
